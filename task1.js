d465ea75c5fee90a71f66ccace618e9c6d10c550
Charan150599 <kcharanteja007@gmail.com>
2026-04-18 12:08:11 +0530

    Add JavaScript tasks for basic operations and types

---
+++ task1.js
@@ -0,0 +1,74 @@
+// task1
+var num =50;
+num = 100;
+console.log(num);
+
+//task2
+let marks =80;
+marks = marks+10;
+console.log(marks);
+
+//task3
+const price = 500;
+const finalprice = price+100;
+console.log(finalprice);
+
+//task4
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+//task5
+console.log(typeof "JavaScript");
+console.log(typeof 250);
+console.log(typeof false);
+
+//task6
+let fruits = ["Apple","Banana","Guava","Orange","Cherry"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+
+//task7
+let student = {
+ name: "Naveen",
+ age: 20,
+ course: "MERN"
+}
+
+console.log(student.name);
+console.log(student.age);
+
+//task8
+console.log(20 + 10);
+console.log(50-25);
+console.log(5*5);
+console.log(100/4);
+console.log(20%3);
+
+//task9
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//task10
+console.log(10 > 5);
+console.log(5 < 2);
+console.log(20 == "20");
+console.log(15 === "15");
+console.log(10 != 8);
+
+//task11
+console.log(5 > 2 && 10 > 3);
+console.log(7 < 5 || 8 > 2);
+console.log(10 > 5);
+
+//task12
+let age = 18;
+let result = age >= 18? "Eligible" : "Not Eligible";
+
+console.log(result);
